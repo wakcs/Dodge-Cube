e46f91d2b509e0aad1e3e69f6bf3853035c2936b
wakcs <geertcocu@hotmail.com>
2014-04-15 20:51:28 +0200

    Import

---
+++ DodgeCube/Assets/FollowObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public Transform target;
+    public int height;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("target is: " + target);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = target.position;
+        transform.position += Vector3.up * height;
+        Vector3 pos = transform.position;
+        pos.x = 0;
+        transform.position = pos;
+	}
+}
